85906c2708696ab7b2e31effaae8e9a8d1b5169c
Andre Schaidhauer Luckmann <andre.luckmann@meta.com.br>
2022-11-22 22:44:12 -0300

    Finished

--- connectMqtt.py
+++ connectMqtt.py
@@ -7,20 +7,20 @@ import json
 import time
 #libs
 
-broker = 'test.mosquitto.org'
+broker = "test.mosquitto.org"
 port = 1883
 #broker adress
 
 topic_read = "Liberato/iotTro/44xx/data"
 topic_send = "Liberato/iotTro/44xx/rply/19000286"
 topic_check = "Liberato/iotTro/44xx/ack/19000286"
-topic_error = "Liberato/iotTro/44xx/ack/"
+topic_error = "Liberato/iotTro/44xx/ack"
 #topic addresses
 
-client_id = 'André Schaidhauer Luckmann'
+client_id = "André Schaidhauer Luckmann"
 seq = 0
-matricula = 19000286
-turma = 4411
+matricula = "19000286"
+turma = "4411"
 alarme = ""
 difTemp = 0
 tempExt = 0
@@ -49,7 +49,7 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         if msg.topic == topic_read:
-            recado = msg.payload.decode('utf-8')
+            recado = msg.payload.decode("utf-8")
             print(f"Received `{recado}` from `{msg.topic}` topic")
 
             jsonDict = json.loads(recado)
@@ -61,10 +61,10 @@ def subscribe(client: mqtt_client):
 
             if tempExt > tempInt:
                 difTemp = round((tempExt - tempInt),1)
-                alarme = "Está mais quente lá fora e há "
+                alarme = "Esta mais quente lá fora e ha "
             else:
                 difTemp = round((tempInt - tempExt),1)
-                alarme = "Está mais frio lá fora e há "
+                alarme = "Esta mais frio lá fora e ha "
 
             if humidade >= 60:
                 alarme += "altas chances de chuva!"
@@ -82,20 +82,23 @@ def subscribe(client: mqtt_client):
             del jsonDict["tempExt"]
             del jsonDict["tempInt"]
             del jsonDict["umidade"]
-            strMsg = str(jsonDict)
+
+            jsonMsg = json.dumps(jsonDict)
+            #Dict to JSON
+
+            strMsg = str(jsonMsg)
+            #json to string
 
             client.publish(topic_send,strMsg)
 
-            print("String Msg:\n",strMsg,"\n")
+            print("\nString Msg:\n",strMsg,"\n")
 
         if msg.topic == topic_check:
-            def on_message(client, userdata, msg):
-                recado = msg.payload.decode('utf-8')
+                recado = msg.payload.decode("utf-8")
                 print(f"\n{recado}`\n")
 
         if msg.topic == topic_error:
-            def on_message(client, userdata, msg):
-                recado = msg.payload.decode('utf-8')
+                recado = msg.payload.decode("utf-8")
                 print(f"\n{recado}`\n")
 
     client.subscribe([(topic_read, 0), (topic_check, 0), (topic_error, 0)]) # Subscribe to Both topics
@@ -103,7 +106,7 @@ def subscribe(client: mqtt_client):
     client.on_message = on_message #Callback to function on_message
 
 def on_publish(client,userdata,result):
-    print(f"Sent Msg!")
+    print(f"Sent Msg!\n")
     pass
 
 
